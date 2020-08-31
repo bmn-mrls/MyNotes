54231fe2e7a44c212dd75e97cd7372ac8ae33df4
Benjamin Morales <bmnmrls@gmail.com>
2020-08-31 00:24:01 -0500

    Add notes adapter to display the list of notes on home

--- app/src/main/java/com/bmnmrls/mynotes/home/views/fragments/HomeFragment.kt
+++ app/src/main/java/com/bmnmrls/mynotes/home/views/fragments/HomeFragment.kt
@@ -5,15 +5,48 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.bmnmrls.mynotes.R
+import com.bmnmrls.mynotes.databinding.FragmentHomeBinding
+import com.bmnmrls.mynotes.home.adapters.NotesAdapter
+import com.bmnmrls.mynotes.home.models.NoteUI
 
 class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+    private val notesAdapter: NotesAdapter by lazy { NotesAdapter() }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        notesAdapter.apply {
+            addNotes(
+                listOf(
+                    NoteUI(0, "Test1", "30/08/20"),
+                    NoteUI(1, "Test2", "30/08/20"),
+                    NoteUI(2, "Test3", "30/08/20"),
+                    NoteUI(3, "Test4", "30/08/20"),
+                    NoteUI(4, "Test5", "30/08/20"),
+                    NoteUI(5, "Test6", "30/08/20")
+                )
+            )
+            listener = {}
+        }
+        binding.notesRecyclerView.apply {
+            setHasFixedSize(true)
+            adapter = notesAdapter
+        }
     }
 
 }
