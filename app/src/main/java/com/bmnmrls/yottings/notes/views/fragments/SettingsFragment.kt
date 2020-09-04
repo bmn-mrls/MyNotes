029af70549c58bcca1c6623c7792a3745daa7348
Benjamin Morales <bmnmrls@gmail.com>
2020-09-04 01:14:09 -0500

    Add settings fragment

---
+++ app/src/main/java/com/bmnmrls/yottings/notes/views/fragments/SettingsFragment.kt
@@ -0,0 +1,22 @@
+package com.bmnmrls.yottings.notes.views.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.bmnmrls.yottings.databinding.FragmentSettingsBinding
+
+class SettingsFragment : Fragment() {
+
+    private lateinit var binding: FragmentSettingsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSettingsBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+}
