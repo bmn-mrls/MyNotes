911a2dfebb28b7c78c5318727fee2368cfc07d23
Benjamin Morales <bmnmrls@gmail.com>
2020-09-05 04:18:14 -0500

    Update button done implementation to be called from activity

--- app/src/main/java/com/bmnmrls/yottings/notes/views/activity/NotesActivity.kt
+++ app/src/main/java/com/bmnmrls/yottings/notes/views/activity/NotesActivity.kt
@@ -3,11 +3,14 @@ package com.bmnmrls.yottings.notes.views.activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.bmnmrls.yottings.R
 import com.bmnmrls.yottings.databinding.ActivityNotesBinding
+import com.bmnmrls.yottings.utils.ktx.hideKeyboard
 import com.bmnmrls.yottings.utils.ktx.setTextFromStringOrResource
 
 class NotesActivity : AppCompatActivity() {
@@ -28,8 +31,15 @@ class NotesActivity : AppCompatActivity() {
             findNavController(R.id.navHostFragment)
         )
         findNavController(R.id.navHostFragment).addOnDestinationChangedListener { _, destination, _ ->
+            evaluateDoneButton(destination)
             binding.toolbarTitleTextView.setTextFromStringOrResource(destination.label.toString())
         }
+    }
+
+    private fun evaluateDoneButton(destination: NavDestination) {
+        binding.doneButton.setOnClickListener { this@NotesActivity.hideKeyboard() }
+        binding.doneButton.visibility =
+            if (destination.id == R.id.createEditNoteFragment) View.VISIBLE else View.GONE
     }
 
     companion object {
