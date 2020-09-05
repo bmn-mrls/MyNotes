f782a25f97416b0e211fe09904f1c3437769ede1
Benjamin Morales <bmnmrls@gmail.com>
2020-09-04 22:52:43 -0500

    Update implementation of extension to be able to add additional space to first and last item

--- app/src/main/java/com/bmnmrls/yottings/utils/VerticalSpaceItemDecoration.kt
+++ app/src/main/java/com/bmnmrls/yottings/utils/VerticalSpaceItemDecoration.kt
@@ -2,11 +2,14 @@ package com.bmnmrls.yottings.utils
 
 import android.graphics.Rect
 import android.view.View
+import androidx.annotation.DimenRes
 import androidx.recyclerview.widget.RecyclerView
 
 class VerticalSpaceItemDecoration(
-    private val verticalSpaceHeight: Int,
-    private val addToLastItem: Boolean = true
+    @DimenRes private val space: Int,
+    private val addToLastItem: Boolean = true,
+    @DimenRes private val firstItemAdditionalSpace: Int? = null,
+    @DimenRes private val lastItemAdditionalSpace: Int? = null
 ) : RecyclerView.ItemDecoration() {
 
     override fun getItemOffsets(
@@ -16,12 +19,34 @@ class VerticalSpaceItemDecoration(
         state: RecyclerView.State
     ) {
         parent.adapter?.let {
-            if (addToLastItem) {
-                outRect.bottom = verticalSpaceHeight
-            } else {
-                if (parent.getChildAdapterPosition(view) != it.itemCount - 1) {
-                    outRect.bottom = verticalSpaceHeight
-                }
+            if (it.itemCount > 0) {
+                setupVerticalSpace(outRect, view, parent, it)
+            }
+        }
+    }
+
+    private fun setupVerticalSpace(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        adapter: RecyclerView.Adapter<RecyclerView.ViewHolder>
+    ) {
+        val childPosition = parent.getChildAdapterPosition(view)
+        if (childPosition == 0) {
+            outRect.top =
+                firstItemAdditionalSpace?.let { view.resources.getDimensionPixelOffset(it) } ?: 0
+        }
+        if (addToLastItem) {
+            outRect.bottom = view.resources.getDimensionPixelOffset(space)
+            if (childPosition == adapter.itemCount - 1) {
+                outRect.bottom =
+                    view.resources.getDimensionPixelOffset(space) + (lastItemAdditionalSpace?.let {
+                        view.resources.getDimensionPixelOffset(it)
+                    } ?: 0)
+            }
+        } else {
+            if (childPosition != adapter.itemCount - 1) {
+                outRect.bottom = view.resources.getDimensionPixelOffset(space)
             }
         }
     }
