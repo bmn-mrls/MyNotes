c29eb9b7d7fc2385bc6487107ea98e5e2b471251
Benjamin Morales <bmnmrls@gmail.com>
2020-09-05 03:39:00 -0500

    Add extension to hide keyboard

--- app/src/main/java/com/bmnmrls/yottings/utils/ktx/Activity.kt
+++ app/src/main/java/com/bmnmrls/yottings/utils/ktx/Activity.kt
@@ -1,7 +1,9 @@
 package com.bmnmrls.yottings.utils.ktx
 
 import android.app.Activity
+import android.content.Context
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 
 fun Activity.makeFullScreen() {
     this.window.decorView.systemUiVisibility = (
@@ -12,4 +14,14 @@ fun Activity.makeFullScreen() {
                     or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                     or View.SYSTEM_UI_FLAG_FULLSCREEN
             )
+}
+
+fun Activity.hideKeyboard() {
+    try {
+        val imm: InputMethodManager? =
+            this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager?
+        imm?.hideSoftInputFromWindow(this.currentFocus?.windowToken, 0)
+    } catch (e: Exception) {
+        //No-op
+    }
 }
