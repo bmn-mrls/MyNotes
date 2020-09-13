a7af502ddd6428fbe9a2b268090dce136a768bec
Benjamin Morales <bmnmrls@gmail.com>
2020-09-13 02:02:03 -0500

    Update implementation repository functions type

--- domain/src/main/java/com/bmnmrls/domain/repositories/NotesRepository.kt
+++ domain/src/main/java/com/bmnmrls/domain/repositories/NotesRepository.kt
@@ -6,14 +6,14 @@ import kotlinx.coroutines.flow.Flow
 
 interface NotesRepository {
 
-    suspend fun createNote(note: Note): Flow<DataState<Long>>
+    fun createNote(note: Note): Flow<DataState<Long>>
 
-    suspend fun updateNote(note: Note): Flow<DataState<Any>>
+    fun updateNote(note: Note): Flow<DataState<Any>>
 
-    suspend fun deleteNote(note: Note): Flow<DataState<Any>>
+    fun deleteNote(note: Note): Flow<DataState<Any>>
 
-    suspend fun getNote(id: Long): Flow<DataState<Note>>
+    fun getNote(id: Long): Flow<DataState<Note>>
 
-    suspend fun getNotes(): Flow<DataState<List<Note>>>
+    fun getNotes(): Flow<DataState<List<Note>>>
 
 }
