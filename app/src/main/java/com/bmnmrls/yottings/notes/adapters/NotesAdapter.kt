cf407cc87061916c474454a76737587a532b48d2
Benjamin Morales <bmnmrls@gmail.com>
2020-09-12 22:39:07 -0500

    Update notes adapter implementation

--- app/src/main/java/com/bmnmrls/yottings/notes/adapters/NotesAdapter.kt
+++ app/src/main/java/com/bmnmrls/yottings/notes/adapters/NotesAdapter.kt
@@ -4,15 +4,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bmnmrls.domain.models.Note
 import com.bmnmrls.yottings.R
 import com.bmnmrls.yottings.databinding.NoteItemLayoutBinding
-import com.bmnmrls.yottings.notes.models.NoteUI
 import com.bmnmrls.yottings.utils.ktx.setTextFromStringOrResource
 
 class NotesAdapter : RecyclerView.Adapter<NotesAdapter.ViewHolder>() {
 
     lateinit var listener: ((Long) -> Unit)
-    private val notes: MutableList<NoteUI> = mutableListOf()
+    private val notes: MutableList<Note> = mutableListOf()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(
         LayoutInflater.from(parent.context).inflate(R.layout.note_item_layout, parent, false)
@@ -23,7 +23,7 @@ class NotesAdapter : RecyclerView.Adapter<NotesAdapter.ViewHolder>() {
 
     override fun getItemCount(): Int = notes.size
 
-    fun addNotes(notes: List<NoteUI>) {
+    fun addNotes(notes: List<Note>) {
         this.notes.clear()
         this.notes.addAll(notes)
         notifyDataSetChanged()
@@ -33,11 +33,11 @@ class NotesAdapter : RecyclerView.Adapter<NotesAdapter.ViewHolder>() {
 
         private val binding: NoteItemLayoutBinding = NoteItemLayoutBinding.bind(view)
 
-        fun bind(item: NoteUI, listener: ((Long) -> Unit)) = with(binding) {
+        fun bind(item: Note, listener: ((Long) -> Unit)) = with(binding) {
             contentTextView.setTextFromStringOrResource(item.content)
             dateTextView.setTextFromStringOrResource(item.date)
             favoriteImageView.visibility = if (item.isFavorite) View.VISIBLE else View.GONE
-            root.setOnClickListener { listener(item.id) }
+            root.setOnClickListener { item.id?.let { itemId -> listener(itemId) } }
         }
 
     }
--- app/src/main/java/com/bmnmrls/yottings/notes/models/NoteUI.kt
+++
@@ -1,8 +0,0 @@
-package com.bmnmrls.yottings.notes.models
-
-data class NoteUI(
-    val id: Long,
-    val content: String,
-    val date: String,
-    val isFavorite: Boolean
-)
