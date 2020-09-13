596841ac0131b440ab839014f246b9bf6eafa3e9
Benjamin Morales <bmnmrls@gmail.com>
2020-09-12 22:31:42 -0500

    Update resources and their use in the project

--- app/src/main/java/com/bmnmrls/yottings/splash/SplashActivity.kt
+++ app/src/main/java/com/bmnmrls/yottings/splash/SplashActivity.kt
@@ -22,18 +22,24 @@ class SplashActivity : AppCompatActivity() {
     private fun setupUI() {
         Timber.tag(TAG).d("setupUI() called")
         binding.splashMotionLayout.setTransitionListener(
-                object : MotionLayout.TransitionListener {
-                    override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) = Unit
+            object : MotionLayout.TransitionListener {
+                override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) = Unit
 
-                    override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) = Unit
+                override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) =
+                    Unit
 
-                    override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
-                        NotesActivity.launch(this@SplashActivity)
-                        finish()
-                    }
-
-                    override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) = Unit
+                override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+                    NotesActivity.launch(this@SplashActivity)
+                    finish()
                 }
+
+                override fun onTransitionTrigger(
+                    p0: MotionLayout?,
+                    p1: Int,
+                    p2: Boolean,
+                    p3: Float
+                ) = Unit
+            }
         )
     }
 
