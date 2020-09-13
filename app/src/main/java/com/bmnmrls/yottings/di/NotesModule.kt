00a0edb8ddbd03ad6cbcfc474595e83febfa0ec1
Benjamin Morales <bmnmrls@gmail.com>
2020-09-13 02:00:19 -0500

    Remove unnecessary dependency provision

--- app/src/main/java/com/bmnmrls/yottings/di/NotesModule.kt
+++ app/src/main/java/com/bmnmrls/yottings/di/NotesModule.kt
@@ -1,8 +1,6 @@
 package com.bmnmrls.yottings.di
 
-import com.bmnmrls.yottings.notes.adapters.NotesAdapter
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.FragmentComponent
 
@@ -10,7 +8,5 @@ import dagger.hilt.android.components.FragmentComponent
 @InstallIn(FragmentComponent::class)
 object NotesModule {
 
-    @Provides
-    fun provideNotesAdapter(): NotesAdapter = NotesAdapter()
 
 }
