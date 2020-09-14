4e9d1933a323655aa558aa700eeb2ad2b767a626
Benjamin Morales <bmnmrls@gmail.com>
2020-09-13 22:24:53 -0500

    Add reverse transform functionality to mappers

--- domain/src/main/java/com/bmnmrls/domain/mappers/Transform.kt
+++ domain/src/main/java/com/bmnmrls/domain/mappers/Transform.kt
@@ -4,7 +4,7 @@ abstract class Transform<T1, T2> {
 
     abstract fun transform(value: T1): T2
 
-    open fun reverseTransform(value: T2): T1 = throw UnsupportedOperationException()
+    abstract fun reverseTransform(value: T2): T1
 
     fun transformCollection(values: List<T1>): List<T2> = values.map { t1 -> transform(t1) }
 
