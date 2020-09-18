30128cc193d7590f161919c870d207b60b7ab90e
Benjamin Morales <bmnmrls@gmail.com>
2020-09-17 21:53:43 -0500

    Add retry event if an error occurs getting notes

--- app/src/main/java/com/bmnmrls/yottings/notes/views/fragments/NotesFragment.kt
+++ app/src/main/java/com/bmnmrls/yottings/notes/views/fragments/NotesFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -60,6 +61,7 @@ class NotesFragment : Fragment() {
     private fun setupUI() {
         setupNotes()
         setupAddNewNote()
+        setupRetryEvent()
     }
 
     private fun setupNotes() {
@@ -100,6 +102,10 @@ class NotesFragment : Fragment() {
             }
         }
     }
+
+    private fun setupRetryEvent() =
+        binding.errorLayout.findViewById<Button>(R.id.retryButton)
+            .setOnClickListener { viewModel.setStateEvent(NotesStateEvent.GetNotes) }
 
     private fun showNotes() {
         binding.notesRecyclerView.showView()
