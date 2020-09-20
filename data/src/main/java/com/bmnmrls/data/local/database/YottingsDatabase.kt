196e63fdf3974b0dc2a3e24f6aaf87c421d99625
Benjamin Morales <bmnmrls@gmail.com>
2020-09-19 19:58:29 -0500

    Add converters class to store types of converters for database

--- data/src/main/java/com/bmnmrls/data/local/database/YottingsDatabase.kt
+++ data/src/main/java/com/bmnmrls/data/local/database/YottingsDatabase.kt
@@ -2,10 +2,13 @@ package com.bmnmrls.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.bmnmrls.data.local.converters.Converters
 import com.bmnmrls.data.local.daos.NotesDao
 import com.bmnmrls.data.local.models.NoteEntity
 
 @Database(entities = [NoteEntity::class], version = 1)
+@TypeConverters(Converters::class)
 abstract class YottingsDatabase : RoomDatabase() {
 
     abstract fun notesDao(): NotesDao
