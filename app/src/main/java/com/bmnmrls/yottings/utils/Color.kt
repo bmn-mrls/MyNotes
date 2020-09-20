23bf1f9db78591376dcb09bd6e7e12cd3d261ecf
Benjamin Morales <bmnmrls@gmail.com>
2020-09-19 20:01:27 -0500

    Add colors definition to be used to generate notes background

---
+++ app/src/main/java/com/bmnmrls/yottings/utils/Color.kt
@@ -0,0 +1,22 @@
+package com.bmnmrls.yottings.utils
+
+import com.bmnmrls.yottings.R
+
+object Color {
+
+    val colors: IntArray = intArrayOf(
+        R.color.gray,
+        R.color.red,
+        R.color.brown,
+        R.color.orange,
+        R.color.yellow,
+        R.color.green,
+        R.color.cyan,
+        R.color.blue_light,
+        R.color.blue_dark,
+        R.color.purple,
+        R.color.violet,
+        R.color.pink
+    )
+
+}
