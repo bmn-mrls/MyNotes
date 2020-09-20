3b4d19e01806a45939f04d8d3aac628340fd1490
Benjamin Morales <bmnmrls@gmail.com>
2020-09-19 20:02:12 -0500

    Change implementation to generate notes background

--- app/src/main/java/com/bmnmrls/yottings/createeditnote/viewmodels/CreateEditNoteViewModel.kt
+++ app/src/main/java/com/bmnmrls/yottings/createeditnote/viewmodels/CreateEditNoteViewModel.kt
@@ -1,7 +1,6 @@
 package com.bmnmrls.yottings.createeditnote.viewmodels
 
 import android.app.Application
-import android.content.Context
 import androidx.hilt.Assisted
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.*
@@ -9,12 +8,11 @@ import com.bmnmrls.domain.DataState
 import com.bmnmrls.domain.Failure
 import com.bmnmrls.domain.models.Note
 import com.bmnmrls.domain.repositories.NotesRepository
-import com.bmnmrls.yottings.R
 import com.bmnmrls.yottings.createeditnote.models.CreateEditNoteStateEvent
+import com.bmnmrls.yottings.utils.ktx.getRandomColorStringPair
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import java.util.*
-import kotlin.random.Random
 
 class CreateEditNoteViewModel @ViewModelInject constructor(
     app: Application,
@@ -46,13 +44,13 @@ class CreateEditNoteViewModel @ViewModelInject constructor(
 
     private fun createNote() {
         val context = getApplication<Application>().applicationContext
-        val noteColors = getRandomColorStringPair(context)
+        val noteColors = context.getRandomColorStringPair()
         val contentCopy = content.value
         val isFavoriteCopy = isFavorite.value
         if (contentCopy != null && isFavoriteCopy != null) {
             val note = Note(
                 content = contentCopy,
-                date = Calendar.getInstance().time.toString(),
+                date = Calendar.getInstance().time,
                 isFavorite = isFavoriteCopy,
                 firstColor = noteColors.first,
                 secondColor = noteColors.second
@@ -66,33 +64,6 @@ class CreateEditNoteViewModel @ViewModelInject constructor(
             _createNoteDataState.value =
                 DataState.Error(Failure.GenericError(IllegalArgumentException()))
         }
-    }
-
-    companion object {
-        private val colors: IntArray = intArrayOf(
-            R.color.gray,
-            R.color.red,
-            R.color.brown,
-            R.color.orange,
-            R.color.yellow,
-            R.color.green,
-            R.color.cyan,
-            R.color.blue_light,
-            R.color.blue_dark,
-            R.color.purple,
-            R.color.violet,
-            R.color.pink
-        )
-
-        private fun getRandomColorStringPair(context: Context): Pair<String, String> {
-            val firstColor = getRandomColorString(context)
-            val secondColor = getRandomColorString(context)
-            if (firstColor == secondColor) getRandomColorStringPair(context)
-            return Pair(firstColor, secondColor)
-        }
-
-        private fun getRandomColorString(context: Context): String =
-            context.getString(colors[Random.nextInt(0, colors.size - 1)])
     }
 
 }
--- app/src/main/java/com/bmnmrls/yottings/utils/ktx/Context.kt
+++ app/src/main/java/com/bmnmrls/yottings/utils/ktx/Context.kt
@@ -5,8 +5,20 @@ import android.graphics.drawable.Drawable
 import androidx.annotation.ColorRes
 import androidx.annotation.DrawableRes
 import androidx.core.content.ContextCompat
+import com.bmnmrls.yottings.utils.Color
+import kotlin.random.Random
 
 fun Context.retrieveColor(@ColorRes colorId: Int): Int = ContextCompat.getColor(this, colorId)
 
 fun Context.retrieveDrawable(@DrawableRes drawableId: Int): Drawable =
     ContextCompat.getDrawable(this, drawableId)!!
+
+fun Context.getRandomColorStringPair(): Pair<String, String> {
+    val firstColor = getRandomColorString()
+    val secondColor = getRandomColorString()
+    if (firstColor == secondColor) getRandomColorStringPair()
+    return Pair(firstColor, secondColor)
+}
+
+fun Context.getRandomColorString(): String =
+    this.getString(Color.colors[Random.nextInt(0, Color.colors.size - 1)])
